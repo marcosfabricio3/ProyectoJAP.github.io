3393c8a37016b2779289532be1082e08e4f1768b
Marcos García <109922862+marcosfabricio3@users.noreply.github.com>
2022-10-17 17:43:24 -0300

    rectificación de un desafíate
    se elimina el desafíate del la entrega 4 por motivos estéticos.

--- js/product-info.js
+++ js/product-info.js
@@ -6,29 +6,71 @@ const URL = `https://japceibal.github.io/emercado-api/products/${localStorage.ge
     .then(data => {
 
         //Texto del Producto y su insercion
-        var text =    `<div class="row">
-        <h1>${data.name}</h1><hr>
-        <h4><strong>Precio: </strong>${data.currency}  ${data.cost}</h4><br><br>
+        var text =    `
+        <br>
+        <div class="row border border-1 p-2 border-opacity-10 rounded">
+        <div class="row">
+        <div class="col"><h1>${data.name}</h1></div>
+        <div class="col"><h1 class="text-end">${data.currency}  ${data.cost}</h1></div>
+        </div>
+        <hr>
+        <br>
         <h4><strong>Descripcion: </strong>${data.description}</h4><br>
         <h4><strong>Categoria: </strong>${data.category}</h4><br><br>
         <h4><Strong>Cantidad de vendidos: </Strong> ${data.soldCount}</h4><br><br>
         <h4><strong>Imagenes ilustrativas</strong></h4><br>
-      </div>`;
+      </div>
+      <br>`;
         
       document.getElementById("insercionProducto").innerHTML = text;
 
-      //Imagenes del producto para su insercion
-      function showResult(data){
-        for (let i = 0; i<= data.images.length; i++){ 
-          let elementHTML = `<div class="col"><img src="${data.images[i]}" alt="" class="img-fluid"></div>`;
-          
-          document.getElementById("insercionImagenProducto").innerHTML += elementHTML;
-        }
+         //Imagenes del producto para su insercion
+         function showResult(data){
+          for (let i = 0; i<= data.images.length; i++){ 
+            let elementHTML = `<div class="col"><img src="${data.images[i]}" alt="" class="img-fluid"></div>`;
+            
+            document.getElementById("insercionImagenProducto").innerHTML += elementHTML;
+          }
+      }
+
+    
+    // Mostrar productos Relacionados
+    
+    function cambiarProducto(relacionadoSeleccionado){
+      localStorage.setItem("identificador del producto", relacionadoSeleccionado);
+      window.location.href = "product-info.html";
     }
+
+    function mostrarProductosRelacionados (){
+  
+      var arrayRelacionados = data.relatedProducts;
+
+      for (let l = 0; l<= arrayRelacionados.length; l++){ 
+        
+        var textRelacionados = `
+        <div class="col-3 border border-start rounded" style="margin: 0px 12px;" onclick=cambiarProducto(${arrayRelacionados[l].id})>
+        <div class="row">
+          <img src="${arrayRelacionados[l].image}" alt="" class="img-fluid">
+        </div>
+        <div class="row"><strong><p>${arrayRelacionados[l].name}</p></strong></div>
+      </div>
+      `;
+        document.getElementById("productosRelacionados").innerHTML += textRelacionados;
+      }
+
+    }
+
+    setTimeout(mostrarProductosRelacionados, 100);
 
     showResult(data);
 
     })
+
+    //Cambiar a Producto Relacionado
+    function cambiarProducto(relacionadoSeleccionado){
+      localStorage.setItem("identificador del producto", relacionadoSeleccionado);
+      window.location.href = "product-info.html";
+    }
 
     //Comentarios
     const URLcomentarios = `https://japceibal.github.io/emercado-api/products_comments/${localStorage.getItem('identificador del producto')}.json`;
@@ -43,18 +85,18 @@ const URL = `https://japceibal.github.io/emercado-api/products/${localStorage.ge
         for (let i = 0; i< data2.length; i++) {
 
              //Calificacion de comentarios Previos
-    var estrellas = document.getElementById("Calificacion").value;
-    if(data2[i].score == 1){
-        var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
-    }else if(data2[i].score == 2){
-        var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
-    }else if(data2[i].score == 3){
-        var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
-    }else if(data2[i].score == 4){
-        var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span>`;
-    }else{
-        var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span>`;
-    };
+                var estrellas = document.getElementById("Calificacion").value;
+                if(data2[i].score == 1){
+                    var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
+                }else if(data2[i].score == 2){
+                    var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
+                }else if(data2[i].score == 3){
+                    var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span> <span class="fa fa-star"></span>`;
+                }else if(data2[i].score == 4){
+                    var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star"></span>`;
+                }else{
+                    var calificacionGuardadas = `<span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span> <span class="fa fa-star checked"></span>`;
+                };
 
 
             let comentarioPredeterminado = `<div class="text-bg-light p-3 border border-secondary rounded-start">
